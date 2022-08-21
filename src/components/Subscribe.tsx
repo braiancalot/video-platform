0399ee1436fc2ab377fa3fc6dd2609b33a7c7604
braiancalot <calot665@hotmail.com>
2022-08-21 16:54:42 -0300

    image ajust 2

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 import { Footer } from "./Footer";
 import { Logo } from "./Logo";
+import codeMockupImage from "../assets/code-mockup.png"
 
 export function Subscribe() {
     const navigate = useNavigate();
@@ -62,7 +63,7 @@ export function Subscribe() {
                         </form>
                     </div>
                 </div>
-                <img src="/assets/code-mockup.png" className="mt-10" alt="" />
+                <img src={codeMockupImage} className="mt-10" alt="" />
             </div>
             <Footer />
         </>
